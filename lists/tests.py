d3b212f20a6ab0ad81871904fd722711125db690
Evan Arbeitman <evan.arbeitman@compass.com>
2018-11-24 09:32:07 -0500

    refactor home page view to use a template add lists app to django settings

--- lists/tests.py
+++ lists/tests.py
@@ -10,9 +10,10 @@ from django.http import HttpRequest
 
 class HomePageTest(TestCase):
 
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
+    def test_uses_home_page(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+
 
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
@@ -21,6 +22,7 @@ class HomePageTest(TestCase):
         self.assertTrue(html.startswith('<html>'))
         self.assertIn(('<title>To-Do lists</title>'), html)
         self.assertTrue(html.endswith('</html>'))
+
 
 
 
